ed7b3f0e41a2ef8309f3bcddc1e22780467a2164
Raisonjosephau <raisonjosephau@gmail.com>
2018-07-08 06:34:08 +0530

    change infowindow style

--- assets/js/fortkochi.js
+++ assets/js/fortkochi.js
@@ -1,4 +1,4 @@
-google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'load', map = init());
 
 function init() {
     // Basic options for a simple Google Map
@@ -8,6 +8,7 @@ function init() {
         zoom: 16,
 
         scrollwheel: false,
+        clickableIcons: false,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(9.9661212, 76.2412716), // New York
@@ -144,32 +145,6 @@ function init() {
         content: contentString
     });
 
-    var activeInfoWindow;
-
-    function setInfoWindow(contentString) {
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-        return infowindow
-    }
-
-    function Location(locName, info, lat, lng) {
-        this.locName = locName;
-        this.info = info;
-        this.lat = lat;
-        this.lng = lng;
-    }
-
-    var data = [
-        new Location('Chinavala', "The great", 9.96839624, 76.2422419),
-        new Location('St. Francis church', "The great", 9.9660689, 76.2408042),
-        new Location('Dutch Cemetry', "The great", 9.9641985, 76.2383446),
-        new Location('Fort Kochi Beach', "The great", 9.963908, 76.237412),
-        new Location('Prince Street', "The great", 9.9668931, 76.2428051),
-        new Location('Jew Town', "The great", 9.955742, 76.260170),
-        new Location('Mattanchery Palace', "The great", 9.958277, 76.259351)
-    ];
-
 
     //
     map = new google.maps.Map(mapElement, mapOptions); // Let's also add a markerwhile we 're at it 
@@ -178,23 +153,85 @@ function init() {
     //     map: map,
     //     animation: google.maps.Animation.DROP,
     // });
+    return map;
 
-    data.forEach(function(d) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(d.lat, d.lng),
-            map: map,
-            animation: google.maps.Animation.DROP,
-        });
-        d.marker = marker;
-    });
-
-
-    $('ul.itenary-steps li').click(function(e) {
-        if (activeInfoWindow) { activeInfoWindow.close(); }
-        console.log(this.getAttribute("data-toggle-map"));
-        map.panTo(data[this.getAttribute("data-toggle-map")].marker.position);
-        infowindow = setInfoWindow(data[5].info);
-        infowindow.open(map, data[this.getAttribute("data-toggle-map")].marker);
-        activeInfoWindow = infowindow;
-    });
 }
+var activeInfoWindow;
+
+function setInfoWindow(contentString) {
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    return infowindow
+}
+
+function Location(locName, info, lat, lng) {
+    this.locName = locName;
+    this.info = info;
+    this.lat = lat;
+    this.lng = lng;
+}
+
+var data = [
+    new Location('Chinavala', "The great", 9.96839624, 76.2422419),
+    new Location('St. Francis church', "The great", 9.9660689, 76.2408042),
+    new Location('Dutch Cemetry', "The great", 9.9641985, 76.2383446),
+    new Location('Fort Kochi Beach', "The great", 9.963908, 76.237412),
+    new Location('Prince Street', "The great", 9.9668931, 76.2428051),
+    new Location('Jew Town', "The great", 9.955742, 76.260170),
+    new Location('Mattanchery Palace', "The great", 9.958277, 76.259351)
+];
+data.forEach(function(d) {
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(d.lat, d.lng),
+        map: map,
+        animation: google.maps.Animation.DROP,
+    });
+    d.marker = marker;
+});
+
+
+
+// InfoWindow content
+var content = '<div id="iw-container">' +
+    '<div class="iw-content">' +
+    '<div class="iw-subTitle">History</div>' +
+    '<p>VISTA ALEGRE ATLANTIS, SA<br>3830-292 Ílhavo - Portugal<br>' +
+    '<br>Phone. +351 234 320 600<br>e-mail: geral@vaa.pt<br>www: www.myvistaalegre.com</p>' +
+    '</div>' +
+    '</div>';
+
+// A new Info Window is created and set content
+
+$('ul.itenary-steps li').click(function(e) {
+    if (activeInfoWindow) { activeInfoWindow.close(); }
+    console.log(this.getAttribute("data-toggle-map"));
+    map.panTo(data[this.getAttribute("data-toggle-map")].marker.position);
+    var infowindow = new google.maps.InfoWindow({
+        content: content,
+
+        maxWidth: 380
+    });
+    infowindow.open(map, data[this.getAttribute("data-toggle-map")].marker);
+
+    // Reference to the DIV that wraps the bottom of infowindow
+    var iwOuter = $('.gm-style-iw');
+
+    /* Since this div is in a position prior to .gm-div style-iw.
+     * We use jQuery and create a iwBackground variable,
+     * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
+     */
+    var iwBackground = iwOuter.prev();
+    iwBackground.hide();
+
+    // Removes background shadow DIV
+    // Reference to the div that groups the close button elements.
+    var iwCloseBtn = iwOuter.next();
+
+    // Apply the desired effect to the close button
+    iwCloseBtn.addClass('btn-info-close');
+    iwCloseBtn.children("img").remove();
+    activeInfoWindow = infowindow;
+});
+
+// The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
