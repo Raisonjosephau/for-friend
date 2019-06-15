e65ed38a674463c5169cb2cc90d668a7d84ee96f
Raisonjosephau <raisonjosephau@gmail.com>
2019-06-15 12:01:07 +0530

    changes

--- data.js
+++ data.js
@@ -1,3 +1,114 @@
 dataWrap = 'W3siaWQiOjAsInRyaXBfbmFtZSI6Ikxlb2xhIFNsb3ZhayBSZXB1YmxpYyIsImRheXMiOjEwLCJwbGFjZXMiOiJCbG9kZ2V0dCAtIEhvbWVsYW5kIC0gUmljaHZpbGxlIiwibGluayI6IiNUaW9nYSIsImNhdGVnb3J5IjoiSG9uZXltb29uIiwiaW1nX3BhdGgiOiJzaG9wLyIsImltZyI6WyJzaG9wLWltZy0xLmpwZyIsInNob3AtaW1nLTYuanBnIiwic2hvcC1pbWctOC5qcGciXX0seyJpZCI6MSwidHJpcF9uYW1lIjoiU3ByaW5ndmlsbGUgT21hbiIsImRheXMiOjIsInBsYWNlcyI6IkF1cm9yYSAtIFdlc3R3b29kIC0gQ2xhcmVuY2UiLCJsaW5rIjoiI0dlcm1hbnRvbiIsImNhdGVnb3J5IjoiV2Vla2VuZCIsImltZ19wYXRoIjoic2hvcC8iLCJpbWciOlsic2hvcC1pbWctMS5qcGciLCJzaG9wLWltZy02LmpwZyIsInNob3AtaW1nLTguanBnIl19LHsiaWQiOjIsInRyaXBfbmFtZSI6IkNsb3ZlcmRhbGUgVGFuemFuaWEiLCJkYXlzIjo1LCJwbGFjZXMiOiJBbGRlbiAtIEdsZW52aWxsZSAtIEJhbG0iLCJsaW5rIjoiI0JhcnRsZXkiLCJjYXRlZ29yeSI6IkRheSBFeGN1cnNpb25zIiwiaW1nX3BhdGgiOiJzaG9wLyIsImltZyI6WyJzaG9wLWltZy0xLmpwZyIsInNob3AtaW1nLTYuanBnIiwic2hvcC1pbWctOC5qcGciXX0seyJpZCI6MywidHJpcF9uYW1lIjoiQm9saW5nIEt1d2FpdCIsImRheXMiOjYsInBsYWNlcyI6IkxpbmRjb3ZlIC0gQnJhbmRlcm1pbGwgLSBCYXJ0b25zdmlsbGUiLCJsaW5rIjoiI0xvcnJhaW5lIiwiY2F0ZWdvcnkiOiJEYXkgRXhjdXJzaW9ucyIsImltZ19wYXRoIjoic2hvcC8iLCJpbWciOlsic2hvcC1pbWctMS5qcGciLCJzaG9wLWltZy02LmpwZyIsInNob3AtaW1nLTguanBnIl19LHsiaWQiOjQsInRyaXBfbmFtZSI6IkJveWtpbiBNb25hY28iLCJkYXlzIjo0LCJwbGFjZXMiOiJHcmFuZHZpZXcgLSBVcmllIC0gQnJvb2t0cmFpbHMiLCJsaW5rIjoiI0JlYXVsaWV1IiwiY2F0ZWdvcnkiOiJXZWVrZW5kIiwiaW1nX3BhdGgiOiJzaG9wLyIsImltZyI6WyJzaG9wLWltZy0xLmpwZyIsInNob3AtaW1nLTYuanBnIiwic2hvcC1pbWctOC5qcGciXX0seyJpZCI6NSwidHJpcF9uYW1lIjoiTmVzY2F0dW5nYSBSZXVuaW9uIiwiZGF5cyI6NSwicGxhY2VzIjoiVHJpbndheSAtIEVsYmVydCAtIEhheWVzIiwibGluayI6IiNUaWNvbmRlcm9nYSIsImNhdGVnb3J5IjoiRGF5IEV4Y3Vyc2lvbnMiLCJpbWdfcGF0aCI6InNob3AvIiwiaW1nIjpbInNob3AtaW1nLTEuanBnIiwic2hvcC1pbWctNi5qcGciLCJzaG9wLWltZy04LmpwZyJdfSx7ImlkIjo2LCJ0cmlwX25hbWUiOiJDb3JuZmllbGRzIFRhaXdhbiIsImRheXMiOjgsInBsYWNlcyI6IkRydW1tb25kIC0gQnJhbWJsZXRvbiAtIE5hc2giLCJsaW5rIjoiI0Nhc2h0b3duIiwiY2F0ZWdvcnkiOiJIb25leW1vb24iLCJpbWdfcGF0aCI6InNob3AvIiwiaW1nIjpbInNob3AtaW1nLTEuanBnIiwic2hvcC1pbWctNi5qcGciLCJzaG9wLWltZy04LmpwZyJdfSx7ImlkIjo3LCJ0cmlwX25hbWUiOiJDbGFya3NidXJnIFR1cmttZW5pc3RhbiIsImRheXMiOjgsInBsYWNlcyI6IlJpdmVyYSAtIEdyZWVuZmllbGRzIC0gQ2hhbWJlcmlubyIsImxpbmsiOiIjU3VubnlzaWRlIiwiY2F0ZWdvcnkiOiJIb25leW1vb24iLCJpbWdfcGF0aCI6InNob3AvIiwiaW1nIjpbInNob3AtaW1nLTEuanBnIiwic2hvcC1pbWctNi5qcGciLCJzaG9wLWltZy04LmpwZyJdfV0 = '
-var elements = window.atob(dataWrap);
-var packages = JSON.parse(elements)
+    // var elements = window.atob(dataWrap);
+let elements = [{
+        "id": 0,
+        "trip_name": "Leola Slovak Republic",
+        "days": 10,
+        "places": "Blodgett - Homeland - Richville",
+        "link": "#Tioga",
+        "category": "Honeymoon",
+        "img_path": "shop/",
+        "img": [
+            "shop-img-1.jpg"
+        ]
+    },
+    {
+        "id": 1,
+        "trip_name": "Springville Oman",
+        "days": 2,
+        "places": "Aurora - Westwood - Clarence",
+        "link": "#Germanton",
+        "category": "Weekend",
+        "img_path": "shop/",
+        "img": [
+            "shop-img-1.jpg",
+            "shop-img-6.jpg",
+            "shop-img-8.jpg"
+        ]
+    },
+    {
+        "id": 2,
+        "trip_name": "Cloverdale Tanzania",
+        "days": 5,
+        "places": "Alden - Glenville - Balm",
+        "link": "#Bartley",
+        "category": "Day Excursions",
+        "img_path": "shop/",
+        "img": [
+            "shop-img-1.jpg",
+            "shop-img-6.jpg",
+            "shop-img-8.jpg"
+        ]
+    },
+    {
+        "id": 3,
+        "trip_name": "Boling Kuwait",
+        "days": 6,
+        "places": "Lindcove - Brandermill - Bartonsville",
+        "link": "#Lorraine",
+        "category": "Day Excursions",
+        "img_path": "shop/",
+        "img": [
+            "shop-img-1.jpg",
+            "shop-img-6.jpg",
+            "shop-img-8.jpg"
+        ]
+    },
+    {
+        "id": 4,
+        "trip_name": "Boykin Monaco",
+        "days": 4,
+        "places": "Grandview - Urie - Brooktrails",
+        "link": "#Beaulieu",
+        "category": "Weekend",
+        "img_path": "shop/",
+        "img": [
+            "shop-img-1.jpg",
+            "shop-img-6.jpg",
+            "shop-img-8.jpg"
+        ]
+    },
+    {
+        "id": 5,
+        "trip_name": "Nescatunga Reunion",
+        "days": 5,
+        "places": "Trinway - Elbert - Hayes",
+        "link": "#Ticonderoga",
+        "category": "Day Excursions",
+        "img_path": "shop/",
+        "img": [
+            "shop-img-1.jpg",
+            "shop-img-6.jpg",
+            "shop-img-8.jpg"
+        ]
+    },
+    {
+        "id": 6,
+        "trip_name": "Cornfields Taiwan",
+        "days": 8,
+        "places": "Drummond - Brambleton - Nash",
+        "link": "#Cashtown",
+        "category": "Honeymoon",
+        "img_path": "shop/",
+        "img": [
+            "shop-img-1.jpg",
+            "shop-img-6.jpg",
+            "shop-img-8.jpg"
+        ]
+    },
+    {
+        "id": 7,
+        "trip_name": "Clarksburg Turkmenistan",
+        "days": 8,
+        "places": "Rivera - Greenfields - Chamberino",
+        "link": "#Sunnyside",
+        "category": "Honeymoon",
+        "img_path": "shop/",
+        "img": [
+            "shop-img-1.jpg",
+            "shop-img-6.jpg",
+            "shop-img-8.jpg"
+        ]
+    }
+]
+var packages = elements;
