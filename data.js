2f742fa825b23f476f656000360ec7ae49661aab
Bhagyalakshmi <bhagyadas96@gmail.com>
2019-06-29 12:59:45 +0530

    package update

--- data.js
+++ data.js
@@ -1,33 +1,22 @@
 dataWrap = 'W3siaWQiOjAsInRyaXBfbmFtZSI6Ikxlb2xhIFNsb3ZhayBSZXB1YmxpYyIsImRheXMiOjEwLCJwbGFjZXMiOiJCbG9kZ2V0dCAtIEhvbWVsYW5kIC0gUmljaHZpbGxlIiwibGluayI6IiNUaW9nYSIsImNhdGVnb3J5IjoiSG9uZXltb29uIiwiaW1nX3BhdGgiOiJzaG9wLyIsImltZyI6WyJzaG9wLWltZy0xLmpwZyIsInNob3AtaW1nLTYuanBnIiwic2hvcC1pbWctOC5qcGciXX0seyJpZCI6MSwidHJpcF9uYW1lIjoiU3ByaW5ndmlsbGUgT21hbiIsImRheXMiOjIsInBsYWNlcyI6IkF1cm9yYSAtIFdlc3R3b29kIC0gQ2xhcmVuY2UiLCJsaW5rIjoiI0dlcm1hbnRvbiIsImNhdGVnb3J5IjoiV2Vla2VuZCIsImltZ19wYXRoIjoic2hvcC8iLCJpbWciOlsic2hvcC1pbWctMS5qcGciLCJzaG9wLWltZy02LmpwZyIsInNob3AtaW1nLTguanBnIl19LHsiaWQiOjIsInRyaXBfbmFtZSI6IkNsb3ZlcmRhbGUgVGFuemFuaWEiLCJkYXlzIjo1LCJwbGFjZXMiOiJBbGRlbiAtIEdsZW52aWxsZSAtIEJhbG0iLCJsaW5rIjoiI0JhcnRsZXkiLCJjYXRlZ29yeSI6IkRheSBFeGN1cnNpb25zIiwiaW1nX3BhdGgiOiJzaG9wLyIsImltZyI6WyJzaG9wLWltZy0xLmpwZyIsInNob3AtaW1nLTYuanBnIiwic2hvcC1pbWctOC5qcGciXX0seyJpZCI6MywidHJpcF9uYW1lIjoiQm9saW5nIEt1d2FpdCIsImRheXMiOjYsInBsYWNlcyI6IkxpbmRjb3ZlIC0gQnJhbmRlcm1pbGwgLSBCYXJ0b25zdmlsbGUiLCJsaW5rIjoiI0xvcnJhaW5lIiwiY2F0ZWdvcnkiOiJEYXkgRXhjdXJzaW9ucyIsImltZ19wYXRoIjoic2hvcC8iLCJpbWciOlsic2hvcC1pbWctMS5qcGciLCJzaG9wLWltZy02LmpwZyIsInNob3AtaW1nLTguanBnIl19LHsiaWQiOjQsInRyaXBfbmFtZSI6IkJveWtpbiBNb25hY28iLCJkYXlzIjo0LCJwbGFjZXMiOiJHcmFuZHZpZXcgLSBVcmllIC0gQnJvb2t0cmFpbHMiLCJsaW5rIjoiI0JlYXVsaWV1IiwiY2F0ZWdvcnkiOiJXZWVrZW5kIiwiaW1nX3BhdGgiOiJzaG9wLyIsImltZyI6WyJzaG9wLWltZy0xLmpwZyIsInNob3AtaW1nLTYuanBnIiwic2hvcC1pbWctOC5qcGciXX0seyJpZCI6NSwidHJpcF9uYW1lIjoiTmVzY2F0dW5nYSBSZXVuaW9uIiwiZGF5cyI6NSwicGxhY2VzIjoiVHJpbndheSAtIEVsYmVydCAtIEhheWVzIiwibGluayI6IiNUaWNvbmRlcm9nYSIsImNhdGVnb3J5IjoiRGF5IEV4Y3Vyc2lvbnMiLCJpbWdfcGF0aCI6InNob3AvIiwiaW1nIjpbInNob3AtaW1nLTEuanBnIiwic2hvcC1pbWctNi5qcGciLCJzaG9wLWltZy04LmpwZyJdfSx7ImlkIjo2LCJ0cmlwX25hbWUiOiJDb3JuZmllbGRzIFRhaXdhbiIsImRheXMiOjgsInBsYWNlcyI6IkRydW1tb25kIC0gQnJhbWJsZXRvbiAtIE5hc2giLCJsaW5rIjoiI0Nhc2h0b3duIiwiY2F0ZWdvcnkiOiJIb25leW1vb24iLCJpbWdfcGF0aCI6InNob3AvIiwiaW1nIjpbInNob3AtaW1nLTEuanBnIiwic2hvcC1pbWctNi5qcGciLCJzaG9wLWltZy04LmpwZyJdfSx7ImlkIjo3LCJ0cmlwX25hbWUiOiJDbGFya3NidXJnIFR1cmttZW5pc3RhbiIsImRheXMiOjgsInBsYWNlcyI6IlJpdmVyYSAtIEdyZWVuZmllbGRzIC0gQ2hhbWJlcmlubyIsImxpbmsiOiIjU3VubnlzaWRlIiwiY2F0ZWdvcnkiOiJIb25leW1vb24iLCJpbWdfcGF0aCI6InNob3AvIiwiaW1nIjpbInNob3AtaW1nLTEuanBnIiwic2hvcC1pbWctNi5qcGciLCJzaG9wLWltZy04LmpwZyJdfV0 = '
     // var elements = window.atob(dataWrap);
-let elements = [{
+let elements = [
+    {
         "id": 0,
-        "trip_name": "Leola Slovak Republic",
-        "days": 10,
-        "places": "Blodgett - Homeland - Richville",
-        "link": "#Tioga",
-        "category": "Honeymoon",
-        "img_path": "shop/",
+        "trip_name": "Theyyam: Dance of Gods",
+        "days": 2,
+        "places": "Kannur - Kozhikode",
+        "link": "#Germanton",
+        "category": "Festival",
+        "img_path": "festivals/",
         "img": [
-            "shop-img-1.jpg"
+            "festival03.jpg",
+            "festival.jpg",
+            "festival04.jpg"
         ]
     },
     {
         "id": 1,
-        "trip_name": "Springville Oman",
-        "days": 2,
-        "places": "Aurora - Westwood - Clarence",
-        "link": "#Germanton",
-        "category": "Weekend",
-        "img_path": "shop/",
-        "img": [
-            "shop-img-1.jpg",
-            "shop-img-6.jpg",
-            "shop-img-8.jpg"
-        ]
-    },
-    {
-        "id": 2,
         "trip_name": "Cloverdale Tanzania",
         "days": 5,
         "places": "Alden - Glenville - Balm",
@@ -41,17 +30,31 @@ let elements = [{
         ]
     },
     {
-        "id": 3,
+        "id": 2,
         "trip_name": "Boling Kuwait",
         "days": 6,
-        "places": "Lindcove - Brandermill - Bartonsville",
+        "places": "Wayanad - Coorg - Calicut",
         "link": "#Lorraine",
-        "category": "Day Excursions",
-        "img_path": "shop/",
+        "category": "Honeymoon",
+        "img_path": "honeymoonpics/",
         "img": [
             "shop-img-1.jpg",
             "shop-img-6.jpg",
             "shop-img-8.jpg"
+        ]
+    },
+    {
+        "id": 3,
+        "trip_name": "Honeymoon in Highranges",
+        "days": 10,
+        "places": "Munnar - Thekkady - Alappuzha",
+        "link": "#Tioga",
+        "category": "Honeymoon",
+        "img_path": "honeymoonpics/",
+        "img": [
+            "thekkady1copy.jpg",
+            "munnar.jpg",
+            "aleppey01.jpg"
         ]
     },
     {
@@ -70,30 +73,30 @@ let elements = [{
     },
     {
         "id": 5,
-        "trip_name": "Nescatunga Reunion",
-        "days": 5,
-        "places": "Trinway - Elbert - Hayes",
-        "link": "#Ticonderoga",
-        "category": "Day Excursions",
-        "img_path": "shop/",
+        "trip_name": "In the midst of Hills",
+        "days": 10,
+        "places": "Athirappally - Munnar - Thekkady - Alappuzha",
+        "link": "#Tioga",
+        "category": "Honeymoon",
+        "img_path": "honeymoonpics/",
         "img": [
-            "shop-img-1.jpg",
-            "shop-img-6.jpg",
-            "shop-img-8.jpg"
+            "aleppey03.jpeg",
+            "munnar13.jpeg",
+            "thekkady04.jpeg"
         ]
     },
     {
         "id": 6,
-        "trip_name": "Cornfields Taiwan",
+        "trip_name": "Kerala Honeymoon",
         "days": 8,
-        "places": "Drummond - Brambleton - Nash",
+        "places": "Munnar - Alappuzha - Kovalam - Poovar",
         "link": "#Cashtown",
         "category": "Honeymoon",
-        "img_path": "shop/",
+        "img_path": "honeymoonpics/",
         "img": [
-            "shop-img-1.jpg",
-            "shop-img-6.jpg",
-            "shop-img-8.jpg"
+            "munnar01.jpg",
+            "aleppey02.jpg",
+            "tvm.jpg"
         ]
     },
     {
@@ -102,7 +105,7 @@ let elements = [{
         "days": 8,
         "places": "Rivera - Greenfields - Chamberino",
         "link": "#Sunnyside",
-        "category": "Honeymoon",
+        "category": "Luxury",
         "img_path": "shop/",
         "img": [
             "shop-img-1.jpg",
